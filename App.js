327d35ed18e3914f12ce7d9e4c3339eaadaf4b3b
VEDLOGIC\jit.sharma <jitendras@vedlogic.com>
2019-11-15 11:44:00 +0530

    Test

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View, CheckBox } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <CheckBox
+        label="REMEMBER ME"
+        onChange={this.clickRemember}
+        accessibilityLabel="21332"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
